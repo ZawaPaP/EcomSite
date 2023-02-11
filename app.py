88d9a8b339f1d6d731e7024a6f69958f7f93c1f1
Kazuhide Izawa <kazuhide@fb.com>
2023-02-11 16:12:35 +0800

    feature stripe csv import

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for
 import hashlib
+import json
+import stripe
 
-app = Flask(__name__, static_folder='./templates/images')
+# This is your test secret API key.
+stripe.api_key = 'sk_test_51MZbIFA9Qashsbv2iR7UnyIYHgP78XhB1gZRqZQ8mozqCCGabK47D3MnIyEzhUiABXvX73CZ2nJZRMbJwgqBYJGE00Cgi5aOoP'
+YOUR_DOMAIN = 'http://localhost:4242'
+
+
+app = Flask(__name__, 
+            static_url_path='',
+            static_folder='templates')
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     if request.method == "POST":
         data = request.form["data"]
-        print(data)
         return redirect (url_for("checkout", data=data))
     return render_template('index.html')
 
 
 @app.route("/checkout", methods = ['GET', 'POST'])
 def checkout():
-    item = request.form.get('select_item')
+    item = json.loads(request.form.get('select_item'))
     return render_template('checkout.html', item = item)
 
 @app.route("/thanks", methods = ['POST'])
@@ -41,7 +49,42 @@ def thanks():
         'address': hash(address),
         'address2': hash(address2)  
     }
+        
     return render_template('thanks.html', data = data, hash_data = hash_data)
+
+
+@app.route("/success")
+def success():
+    return render_template('success.html')
+
+@app.route("/cancel")
+def cancel():
+    return render_template('cancel.html')
+
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': 'price_1MZvCsA9Qashsbv2nKMBjfu7',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success.html',
+            cancel_url=YOUR_DOMAIN + '/cancel.html',
+            automatic_tax={'enabled': False},
+        )
+    except Exception as e:
+        return str(e)
+
+    return redirect(checkout_session.url, code=303)
 
 def hash(string):
     return hashlib.sha256(string.encode('utf-8')).hexdigest()
+
+if __name__ == '__main__':
+    app.run(port=4242)
---
+++ stripe_csv_to_db.py
@@ -0,0 +1,46 @@
+# Import required modules
+import csv
+import sqlite3
+import pandas as pd
+
+def init():
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    sql = """CREATE TABLE IF NOT EXISTS products (
+        id integer primary key autoincrement,
+        product_id, 
+        name, 
+        description,
+        price_id,
+        value,
+        currency)"""
+    cursor.execute(sql)
+    conn.commit()
+    conn.close()
+
+
+def add_data():
+    conn = sqlite3.connect('products.db', isolation_level=None)
+
+    df_product = pd.read_csv('csv/products.csv', usecols=[0, 1, 4])
+    df_price = pd.read_csv('csv/prices.csv', usecols=[0, 1, 7, 8])
+
+    df = df_product.merge(df_price, how='inner', left_on='id', right_on='Product ID')
+    #delete duplicated column
+    df = df.drop(columns=['id'])
+    #Pandas to_sql
+    df.to_sql(name='products', con=conn, if_exists='replace')
+    conn.close()
+
+def check_data():
+    conn = sqlite3.connect('stripe.db', isolation_level=None)
+    cursor = conn.cursor()
+    sql = """SELECT * FROM products"""
+    cursor.execute(sql)
+    
+    #全レコードを取り出す
+    print(cursor.fetchall())
+    conn.close()
+
+add_data()
+
